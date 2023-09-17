e49cf3be26d01427dcb0596511cf27fc5ded6134
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2023-09-17 00:15:36 -0400

    Login bg dev

--- src/components/WindowWrapper/WindowWrapper.tsx
+++ src/components/WindowWrapper/WindowWrapper.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Flex } from '@chakra-ui/react';
 
 type Props = {
-    children?: React.ReactNode
-    bg?: string
+    children?: React.ReactNode;
+    bg?: string;
+    cls?: string;
 };
 
-const WindowWrapper:React.FC<Props> = ({ children, bg }) => {
+const WindowWrapper:React.FC<Props> = ({ children, bg, cls }) => {
     
     return (
-        <Flex align='center' direction="column" w='100%' bg={bg ?? 'none'}>  
+        <Flex zIndex={1} align='center' direction="column" w='100%'>  
+            <Flex className={cls} w='100%' h='100%'>
+
+            </Flex>
             <Flex justify='center' direction="column" w='100%' maxW='1080px'>
                 {children}
             </Flex>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,13 +3,17 @@ import { Flex, Text } from '@chakra-ui/react';
 import WindowWrapper from '@/components/WindowWrapper/WindowWrapper';
 export default function Login() {
     return (
-        <WindowWrapper bg='radial-gradient(circle, rgba(252,55,46,1) 0%, rgba(250,140,119,1) 100%)'>
-            <Flex w='100%' h='600px'>
-                <Flex direction='column' w='33.33%' h='100%' pt={40}>
-                    <Text color='white' fontFamily='SFPROB1'>Welcome Back</Text>
+        <WindowWrapper cls="noise">
+            <Flex className="noise-child"  zIndex={2} w='100%' h='600px'>
+                <Flex direction='column' display={['none', 'none', 'flex', 'flex']} w='33.33%' h='100%' pt={40}>
+                    <Text ml={5} color='white' fontFamily='SFPROBOLD' fontSize='24pt'>Quarrel</Text>
+                    <Text ml={5} color='white' fontFamily='SFPROBOLD' fontSize='24pt'>QUARREL</Text>
+                    <Text ml={5} color='white' fontFamily='SFPROBOLD' fontSize='24pt'>Welcome Back.</Text>
                 </Flex>
-                <Flex direction='column' w='66.66%' h='100%' bg='white'>
-
+                <Flex zIndex={5} align='center' justify='center' direction='column' w={['100%', '100%', '66.66%', '66.66%']} h='100%' bg='rgba(255, 255, 255, 0.2);' border='1px solid rgba(255, 255, 255, 0.1);' shadow='0 4px 30px rgba(0, 0, 0, 0.1);' backdropFilter='blur(0.4px);'>
+                    <Flex w='440px' h='540px' p={7} bg='white' border='1.5px solid #E5E5E3' borderRadius={10} style={{filter: 'none !important;'}}>
+                        <Text fontFamily='SFPROB1' fontSize='24pt'>Login</Text>
+                    </Flex>
                 </Flex>
             </Flex>
             
